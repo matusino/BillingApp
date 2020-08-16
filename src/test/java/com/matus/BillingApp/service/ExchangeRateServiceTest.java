61dd0cc6012d33a6b7212928c2761010be728f63
Matus <matusino.sykora@gmail.com>
2020-08-16 18:02:23 +0200

    ExchangeRateService test added

---
+++ src/test/java/com/matus/BillingApp/service/ExchangeRateServiceTest.java
@@ -0,0 +1,12 @@
+package com.matus.BillingApp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExchangeRateServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
