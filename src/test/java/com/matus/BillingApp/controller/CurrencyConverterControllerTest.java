93a70e30dee4c973c776563d966186bbeef8199d
Matus <matusino.sykora@gmail.com>
2020-08-18 21:15:26 +0200

    CurrencyConverterController- test class added

---
+++ src/test/java/com/matus/BillingApp/controller/CurrencyConverterControllerTest.java
@@ -0,0 +1,21 @@
+package com.matus.BillingApp.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyConverterControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void convertUsdToZar() {
+    }
+
+    @Test
+    void convertZarToUsd() {
+    }
+}
